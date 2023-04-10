4c3023ec1d1893d8313e0681d67a3f93706efe69
xgy2 <130290954+xgy2@users.noreply.github.com>
2023-04-10 22:33:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -65,3 +65,14 @@ async function bootstrap() {
   });
 }
 bootstrap();
+router.post('/message/post', async ctx => {
+  const { ToUserName, FromUserName, Content, CreateTime } = ctx.request.body;
+
+  ctx.body = {
+    ToUserName: FromUserName,
+    FromUserName: ToUserName,
+    CreateTime: +new Date(),
+    MsgType: 'text',
+    Content: `反弹你发的消息：${Content}`,
+  };
+});
